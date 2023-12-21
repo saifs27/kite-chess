ef43947a73ecb126dd4eb7e9e58492733dad5ea6
Saif <113651806+saifs27@users.noreply.github.com>
2023-12-21 00:22:10 -0600

    pragma once

--- src/movegen.hpp
+++ src/movegen.hpp
@@ -1,12 +1,13 @@
+#pragma once
 #include "board.hpp"
 
 class Move {
     public:
-    void quiet(const BitBoard position, int move);
-    void capture(const BitBoard position, int move);
-    void en_passant(const BitBoard position, int move);
-    void quiet_move(const BitBoard position, int move);
-    void white_pawn_capture(const BitBoard position, int move);
-    void white_pawn(const BitBoard position, int move);
-    void all_moves(const BitBoard position, int move);
+    void quiet(const BitBoard position, const int move);
+    void capture(const BitBoard position, const int move);
+    void en_passant(const BitBoard position, const int move);
+    void quiet_move(const BitBoard position, const int move);
+    void white_pawn_capture(const BitBoard position, const int move);
+    void white_pawn(const BitBoard position, const int move);
+    void all_moves(const BitBoard position, const int move);
 };
